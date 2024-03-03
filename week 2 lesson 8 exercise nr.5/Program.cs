0b96b508cfb6d9bf00f697fa6e036b6df5664e4c
konrad101296 <konradslimak@o2.pl>
2024-03-03 19:29:10 +0100

    Add project files.

---
+++ week 2 lesson 8 exercise nr.5/Program.cs
@@ -0,0 +1,9 @@
+﻿double wynik = 0;
+
+for (int i = 1; i <= 20; i++)
+{
+    double potega = Math.Pow(i, 2);
+    potega = potega * i;
+    Console.WriteLine($"liczba {i} do potegi trzeciej to: {potega}");
+
+}
