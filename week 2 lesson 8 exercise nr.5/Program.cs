d5d735b525167887395ae46a4e1f874c60d1f706
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-19 19:56:02 +0100

    Update Program.cs

--- week 2 lesson 8 exercise nr.5/Program.cs
+++ week 2 lesson 8 exercise nr.5/Program.cs
@@ -1,9 +1,5 @@
-﻿double wynik = 0;
-
 for (int i = 1; i <= 20; i++)
 {
-    double potega = Math.Pow(i, 2);
-    potega = potega * i;
-    Console.WriteLine($"liczba {i} do potegi trzeciej to: {potega}");
-
+    double power = Math.Pow(i, 3);
+    Console.WriteLine($"Number {i} to the third power is: {power}");
 }
